7014d9db30d62a354c0cf3b421ad2bf0cfee95bd
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-08-27 13:13:26 -0700

    fix test-deno

--- bindings/deno/mod.ts
+++ bindings/deno/mod.ts
@@ -6,20 +6,20 @@ const BASE = `${meta.github}/releases/download/v${meta.version}`;
 // Similar to https://github.com/denodrivers/sqlite3/blob/f7529897720631c2341b713f0d78d4d668593ea9/src/ffi.ts#L561
 let path: string;
 try {
-  const customPath = Deno.env.get("DENO_SQLITE_robotstxt_PATH");
+  const customPath = Deno.env.get("DENO_SQLITE_ROBOTSTXT_PATH");
   if (customPath) path = customPath;
   else {
     path = await download({
       url: {
         darwin: {
-          aarch64: `${BASE}/sqlite-lines-v${meta.version}-deno-darwin-aarch64.robotstxt0.dylib`,
-          x86_64: `${BASE}/sqlite-lines-v${meta.version}-deno-darwin-x86_64.robotstxt0.dylib`,
+          aarch64: `${BASE}/sqlite-robotstxt-v${meta.version}-deno-darwin-aarch64.robotstxt0.dylib`,
+          x86_64: `${BASE}/sqlite-robotstxt-v${meta.version}-deno-darwin-x86_64.robotstxt0.dylib`,
         },
         windows: {
-          x86_64: `${BASE}/sqlite-lines-v${meta.version}-deno-windows-x86_64.robotstxt0.dll`,
+          x86_64: `${BASE}/sqlite-robotstxt-v${meta.version}-deno-windows-x86_64.robotstxt0.dll`,
         },
         linux: {
-          x86_64: `${BASE}/sqlite-lines-v${meta.version}-deno-linux-x86_64.robotstxt0.so`,
+          x86_64: `${BASE}/sqlite-robotstxt-v${meta.version}-deno-linux-x86_64.robotstxt0.so`,
         },
       },
       suffixes: {
